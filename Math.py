a396643cfbd005c0d924466675660d93b63ee808
Dishank <dishankagarwa123@gmail.com>
2025-08-28 21:05:53 +0530

    Second Commit

---
+++ Math.py
@@ -0,0 +1,9 @@
+x = 10
+y = 3
+print(x + y)   # Addition
+print(x - y)   # Subtraction
+print(x * y)   # Multiplication
+print(x / y)   # Division
+print(x // y)  # Floor division
+print(x % y)   # Remainder
+print(x ** y)  # Power
